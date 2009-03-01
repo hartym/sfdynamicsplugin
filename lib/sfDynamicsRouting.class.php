c49e3d6fab31b42bf3974b0806b6c13ac75bebcd
hartym <hartym@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-03-01 19:30:36 +0000

    [sfDynamicsPlugin] added symfony version check in routing configuration to use either old style sfPatternRouting or the new style sfRoute objects.
    git-svn-id: http://svn.symfony-project.com/plugins/sfDynamicsPlugin/trunk@15904 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- lib/sfDynamicsRouting.class.php
+++ lib/sfDynamicsRouting.class.php
@@ -1,30 +1,135 @@
 <?php
 
+/**
+ * sfDynamicsRouting - configures the routing for the plugin
+ *
+ * @package sfDynamicsPlugin
+ * @version SVN: $Id: $
+ * @author  Romain Dorgueil <romain.dorgueil@symfony-project.com>
+ * @license MIT
+ */
 class sfDynamicsRouting
 {
-  const ROUTE = 'sfDynamics';
+  /**
+   * sfDynamicsRouting::PLUGIN_NAME - this is the namespace for app.yml configuration
+   */
+  const PLUGIN_NAME = 'sfDynamicsPlugin';
 
+  /**
+   * sfDynamicsRouting::ROUTE - prefix for all routes comming from sfDynamicsPlugin
+   */
+  const ROUTE       = 'sfDynamics';
+
+  /**
+   * Available asset types, and their associated extensions.
+   */
   static protected $types = array(
-    'javascript'=>'js',
-    'stylesheet'=>'css'
+    'javascript' => 'js',
+    'stylesheet' => 'css'
     );
 
-  static public function configure(sfEvent $e)
+  /**
+   * Stores whether routes use sfRoute objects or not.
+   */
+  static protected $newStyleRoutes;
+
+  /**
+   * checkSymfonyVersion - checks if symfony version used is compatible with the
+   * plugin, and detect whether routes are new style (using sfRoute objects) or
+   * old style.
+   *
+   * @return void
+   */
+  static protected function checkSymfonyVersion()
   {
-    $r = $e->getSubject();
-    $prefix = sfConfig::get('app_sfDynamicsPlugin_base_route', '/dynamics');
+    if (defined('SYMFONY_VERSION'))
+    {
+      list($sfVersionMajor, $sfVersionMinor, $sfVersionRelease) = explode('.', SYMFONY_VERSION);
+
+      if (($sfVersionMajor!=1) || (!in_array($sfVersionMinor, array(1,2,3))))
+      {
+        throw new sfConfigurationException(self::PLUGIN_NAME.' needs symfony 1.1 to 1.3 to run.');
+      }
+
+      self::$newStyleRoutes = (bool)($sfVersionMinor>1);
+    }
+    else
+    {
+      throw new sfConfigurationException(self::PLUGIN_NAME.' needs symfony 1.1 to 1.3 to run, but no version were found.');
+    }
+  }
+
+  /**
+   * addRoute - prepend a route to given routing object with abstraction of
+   * symfony version
+   *
+   * @param  sfRouting $r
+   * @param  string $routeName
+   * @param  string $routeUrl
+   * @param  array $routeParameters
+   * @return void
+   */
+  static protected function addRoute(sfRouting $r, $routeName, $routeUrl, $routeParameters)
+  {
+    if (self::$newStyleRoutes)
+    {
+      $r->prependRoute(self::ROUTE.'_'.$routeName, new sfRoute($routeUrl, $routeParameters));
+    }
+    else
+    {
+      $r->prependRoute(self::ROUTE.'_'.$routeName, $routeUrl, $routeParameters);
+    }
+  }
+
+  /**
+   * configure - configures the routing when main project will load it
+   *
+   * @listen routing.load_configuration
+   *
+   * @param  sfEvent $e
+   * @return void
+   */
+  static public function configure(sfEvent $event)
+  {
+    self::checkSymfonyVersion();
+
+    $routing = $event->getSubject();
+    $prefix = sfConfig::get('app_'.self::PLUGIN_NAME.'_base_route', '/dynamics');
 
     foreach (self::$types as $actionName => $fileExtension)
     {
-      $r->prependRoute(self::ROUTE.'_'.$actionName, new sfRoute($prefix.'/:name.'.$fileExtension, array('module' => 'sfDynamics', 'action'=>$actionName)));
+      self::addRoute(
+        $routing,
+        $actionName,
+        $prefix.'/:name.'.$fileExtension,
+        array(
+          'module' => 'sfDynamics',
+          'action' => $actionName
+        )
+      );
 
       foreach (array('language'=>'/l/:language', 'theme'=>'/t/:theme', 'language_theme'=> '/l/:language/t/:theme') as $routeName => $routeEnhancer)
       {
-        $r->prependRoute(self::ROUTE.'_'.$actionName.'_'.$routeName, new sfRoute($prefix.$routeEnhancer.'/:name.'.$fileExtension, array('module' => 'sfDynamics', 'action'=>$actionName)));
+        self::addRoute(
+          $routing,
+          $actionName.'_'.$routeName,
+          $prefix.$routeEnhancer.'/:name.'.$fileExtension,
+          array(
+            'module' => 'sfDynamics',
+            'action' => $actionName
+          )
+        );
       }
     }
   }
 
+  /**
+   * uri_for - builds a symfony URI from an asset name and its extension
+   *
+   * @param  string $name
+   * @param  string $extension
+   * @return string
+   */
   static public function uri_for($name, $extension)
   {
     $translator = array_flip(self::$types);
@@ -37,6 +142,13 @@ class sfDynamicsRouting
     return '@'.self::ROUTE.'_'.$translator[$extension].'?name='.str_replace('.', '-', $name);
   }
 
+  /**
+   * supercache_for - build supercache filename or given packagelist and extension
+   *
+   * @param  string $packages
+   * @param  string $extension
+   * @return string
+   */
   static public function supercache_for($packages, $extension)
   {
     $cacheKey = '';
