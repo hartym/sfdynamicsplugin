eb12b16b79f67a307ff364483fad54d733793ae6
hartym <hartym@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-03-10 14:48:10 +0000

    [sfDynamicsPlugin] removed debug mode
    git-svn-id: http://svn.symfony-project.com/plugins/sfDynamicsPlugin/trunk@16143 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- data/js/jquery/1.3.2/ui/ui.datepicker.js
+++ data/js/jquery/1.3.2/ui/ui.datepicker.js
@@ -23,7 +23,7 @@ var PROP_NAME = 'datepicker';
    allowing multiple different settings on the same page. */
 
 function Datepicker() {
-	this.debug = true; // Change this to true to start debugging
+	this.debug = false; // Change this to true to start debugging
 	this._curInst = null; // The current instance in use
 	this._keyEvent = false; // If the last event was a key event
 	this._disabledInputs = []; // List of date picker inputs that have been disabled
