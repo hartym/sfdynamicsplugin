d1344a1c2a434afd10729bb62b141d66f78dff11
hartym <hartym@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2009-05-30 19:32:10 +0000

    [sfDynamicsPlugin] switched the web debug toolbar into plugin configuration class to avoid compatibility problems with symfony 1.1
    git-svn-id: http://svn.symfony-project.com/plugins/sfDynamicsPlugin/trunk@18786 ee427ae8-e902-0410-961c-c3ed070cd9f9

--- config/sfDynamicsPluginConfiguration.class.php
+++ config/sfDynamicsPluginConfiguration.class.php
@@ -16,5 +16,8 @@ class sfDynamicsPluginConfiguration extends sfPluginConfiguration
   public function initialize()
   {
     require dirname(__FILE__).'/config.php';
+    require_once dirname(__FILE__).'/../lib/debug/sfWebDebugPanelDynamics.class.php';
+
+    $this->dispatcher->connect('debug.web.load_panels', array('sfWebDebugPanelDynamics', 'listenToLoadPanelEvent'));
   }
 }
