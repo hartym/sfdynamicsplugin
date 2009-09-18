3d30a96674a99ecc71ec763793bf4991041f696a
Romain Dorgueil <romain.dorgueil@symfony-project.com>
2009-09-18 09:25:28 +0000

    [sfDynamicsPlugin] fix ticket #11 (http://trackeet.net/issues/show/11) - More error check and exceptions if supercache directory does not exist.

--- config/config.php
+++ config/config.php
@@ -11,7 +11,13 @@ if (!is_writeable(sfDynamicsCache::getSuperCacheDir(true)))
     throw new sfConfigurationException('sfDynamicsPlugin supercache directory does not exists, and contains «..» components. Please check your configuration, or that «'.$_superCacheDir.'» is writeable.');
   }
 
-  mkdir($_superCacheDir, 0777, true);
-  chmod($_superCacheDir, 0777);
+  if (!@mkdir($_superCacheDir, 0777, true))
+  {
+    throw new Exception(sprintf('Could not create dir «%s», check the permissions', $_superCacheDir));
+  }
+  if (!@chmod($_superCacheDir, 0777))
+  {
+    throw new Exception(sprintf('Could not change owner dir «%s», check the permissions', $_superCacheDir));
+  }
 }
 
