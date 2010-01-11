b6d7f79e64adde484966430108677f5d2c2896cf
Romain Dorgueil <romain.dorgueil@symfony-project.com>
2010-01-11 08:54:49 +0100

    [sfDynamicsPlugin] minor tuning to make test suite pass under 1.2, 1.3, and 1.4 symfony versions

--- config/config.php
+++ config/config.php
@@ -2,22 +2,26 @@
 $this->dispatcher->connect('routing.load_configuration', array('sfDynamicsRouting', 'configure'));
 $this->dispatcher->connect('task.cache.clear', array('sfDynamicsCache', 'clearSuperCache'));
 
-if (!is_writeable(sfDynamicsCache::getSuperCacheDir(true)))
+
+if (class_exists('sfDynamicsCache'))
 {
-  $_superCacheDir = sfDynamicsCache::getSuperCacheDir(true);
+  if (!is_writeable(sfDynamicsCache::getSuperCacheDir(true)))
+  {
+    $_superCacheDir = sfDynamicsCache::getSuperCacheDir(true);
 
-  if (false!==strpos($_superCacheDir, '..'))
-  {
-    throw new sfConfigurationException('sfDynamicsPlugin supercache directory does not exists, and contains «..» components. Please check your configuration, or that «'.$_superCacheDir.'» is writeable.');
-  }
+    if (false!==strpos($_superCacheDir, '..'))
+    {
+      throw new sfConfigurationException('sfDynamicsPlugin supercache directory does not exists, and contains «..» components. Please check your configuration, or that «'.$_superCacheDir.'» is writeable.');
+    }
 
-  if (!@mkdir($_superCacheDir, 0777, true))
-  {
-    throw new Exception(sprintf('Could not create dir «%s», check the permissions', $_superCacheDir));
-  }
-  if (!@chmod($_superCacheDir, 0777))
-  {
-    throw new Exception(sprintf('Could not change owner dir «%s», check the permissions', $_superCacheDir));
+    if (!@mkdir($_superCacheDir, 0777, true))
+    {
+      throw new Exception(sprintf('Could not create dir «%s», check the permissions', $_superCacheDir));
+    }
+    if (!@chmod($_superCacheDir, 0777))
+    {
+      throw new Exception(sprintf('Could not change owner dir «%s», check the permissions', $_superCacheDir));
+    }
   }
 }
 
